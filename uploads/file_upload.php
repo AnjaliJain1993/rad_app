e29eac6af225608fdea54b92a77831422c993308
radius <anjalijain5094@gmail.com>
2016-08-13 12:20:12 +0000

    fifth commit

--- uploads/file_upload.php
+++ uploads/file_upload.php
@@ -1,10 +1,11 @@
 <?php
   $filename=$_GET['filename'];
-//  $f="/var/www/html/rad_app/uploads/".$filename;
+//  $f='/var/www/html/rad_app/uploads/'.$filename;
   $fileData=file_get_contents('php://input');
   $fhandle=fopen($filename, 'wb');
   fwrite($fhandle, $fileData);
   fclose($fhandle);
+//  chmod($f,0777);
 ?>
 
 
